02c51896180ed56ba2a86ac9075f39972c65b9eb
STS <hieunguyen.13092000@gmail.com>
2023-10-10 12:21:22 +0700

    analyses

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import './globals.css';
 import type { Metadata } from 'next';
+import { Analytics } from '@vercel/analytics/react';
 
 export const metadata: Metadata = {
   title: 'HƯƠNG THẢO MỘC',
@@ -14,6 +15,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <>
           <div id="modal-root"></div>
         </>
+        <Analytics />
       </body>
     </html>
   );
