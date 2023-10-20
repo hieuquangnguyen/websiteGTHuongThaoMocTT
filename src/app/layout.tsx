36a3d00c1d73952a1d07e740cb4f1380d4411744
STS <hieunguyen.13092000@gmail.com>
2023-10-20 19:14:00 +0700

    edit title tab

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,17 +2,15 @@ import './globals.css';
 import type { Metadata } from 'next';
 import { Analytics } from '@vercel/analytics/react';
 
-
-
 export const metadata: Metadata = {
-  title: 'HƯƠNG THẢO MỘC',
-  description: 'Hương Thảo Dược, Natural incense, Nhang Tự Nhiên, Hương Thảo Mộc',
+  title: 'THẢO MỘC HƯƠNG T&T',
+  description:
+    'Hương Thảo Dược, Natural incense, Nhang Tự Nhiên, Hương Thảo Mộc, Thảo Mộc Hương, huong tu nhien, hương sạch, nhang sạch',
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html>
-      
       <body>
         <>{children}</>
         <>
